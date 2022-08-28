25d20c656d9b14ddae7aaa21c5c29069c21cd2cb
vhdedic <vdedic@gmail.com>
2022-08-28 11:06:10 +0200

    Change user table unique key

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
+    #[ORM\Column(length: 180)]
     private ?string $username = null;
 
     #[ORM\Column]
@@ -27,7 +27,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     public function getId(): ?int
