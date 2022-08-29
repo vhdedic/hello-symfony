353e85e93b0d6a148cc6f33335748cb2a655308e
vhdedic <vdedic@gmail.com>
2022-08-29 17:08:42 +0200

    Post create form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,15 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostFormType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $em, PostRepository $postRepository)
+    {
+        $this->em = $em;
+        $this->postRepository = $postRepository;
+    }
+    
     #[Route('/', name: 'app_post_index')]
     public function index(): Response
     {
         return $this->render('post/index.html.twig');
+    }
+
+    #[Route('/post/create', name: 'app_post_create')]
+    public function create(Request $request): Response
+    {
+        $post = new Post;
+
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $post->setTitle($form->get('title')->getData());
+            $post->setBody($form->get('body')->getData());
+            $post->setUser($this->getUser());
+            
+            $this->em->persist($post);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_post_index');
+        } 
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 class Post
@@ -16,9 +17,11 @@ class Post
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank()]
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
+    #[Assert\NotBlank()]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $body = null;
 
